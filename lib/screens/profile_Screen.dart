50d6836e3b692b889b75f5553894119dc53ab1f0
ZAIN ALI <97796695+mrZAIN-ALI@users.noreply.github.com>
2023-10-06 18:08:41 +0500

    done with user profile page

--- lib/screens/profile_Screen.dart
+++ lib/screens/profile_Screen.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:chit_chat/models/user.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class ProfileScreen extends StatefulWidget {
   final chatUUser_Info user_Info;
@@ -11,120 +12,9 @@ class ProfileScreen extends StatefulWidget {
   State<ProfileScreen> createState() => _ProfileScreenState();
 }
 
-// class _ProfileScreenState extends State<ProfileScreen> {
-//   @override
-//   Widget build(BuildContext context) {
-//     //
-//     final mediaQ = MediaQuery.of(context).size;
-//     final height_forCircualrImage =
-//         mediaQ.height * 0.20; // Change the height here
-//     final FocusScopeNode nameFoucsNode = FocusScopeNode();
-//     final FocusScopeNode aboutFocusNode = FocusScopeNode();
-//     //
-//     return Scaffold(
-//       appBar: AppBar(
-//         leading: Icon(Icons.home_outlined),
-//         title: Text("User Profile"),
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(8.0),
-//         child: Column(
-//           children: [
-//             SizedBox(
-//               height: mediaQ.height * 0.020,
-//               width: mediaQ.width,
-//             ),
-//             ClipRRect(
-//               borderRadius: BorderRadius.circular(height_forCircualrImage / 2),
-//               child: CachedNetworkImage(
-//                 fit: BoxFit.cover,
-//                 height: height_forCircualrImage,
-//                 width:
-//                     height_forCircualrImage, // Set both height and width to the same value
-//                 imageUrl: widget.user_Info.image ??
-//                     "http://via.placeholder.com/350x150",
-//                 progressIndicatorBuilder: (context, url, downloadProgress) =>
-//                     CircularProgressIndicator(value: downloadProgress.progress),
-//                 errorWidget: (context, url, error) =>
-//                     Icon(CupertinoIcons.person),
-//               ),
-//             ),
-
-//             //
-//             SizedBox(
-//               height: mediaQ.height * 0.020,
-//             ),
-//             Text(widget.user_Info.email,
-//                 style: TextStyle(color: Colors.black54)),
-//             //
-//             SizedBox(
-//               height: mediaQ.height * 0.020,
-//             ),
-//             Padding(
-//               padding: const EdgeInsets.all(8.0),
-//               child: TextFormField(
-//                 focusNode: nameFoucsNode,
-//                 initialValue: widget.user_Info.name,
-//                 decoration: InputDecoration(
-//                   // focusColor: Theme.of(context).colorScheme.primary,
-//                   enabledBorder: OutlineInputBorder(
-//                     borderRadius: BorderRadius.circular(15),
-//                     borderSide: BorderSide(color: Colors.black54),
-//                   ),
-//                   hintText: "i.e : John Smith",
-//                   prefixIcon: Icon(
-//                     CupertinoIcons.person,
-//                     color: nameFoucsNode.hasFocus
-//                         ? Theme.of(context).colorScheme.primary
-//                         : Colors.black54,
-//                   ),
-//                   label: const Text(
-//                     "Name",
-//                     style: TextStyle(fontSize: 25),
-//                   ),
-//                 ),
-//               ),
-//             ),
-
-//             SizedBox(
-//               height: mediaQ.height * 0.020,
-//             ),
-//             Padding(
-//               padding: const EdgeInsets.all(8.0),
-//               child: TextFormField(
-//                 focusNode: aboutFocusNode,
-//                 initialValue: widget.user_Info.about,
-//                 decoration: InputDecoration(
-//                   enabledBorder: OutlineInputBorder(
-//                     borderRadius: BorderRadius.circular(15),
-//                     borderSide: BorderSide(color: Colors.black54),
-//                   ),
-//                   hintText: "i.e : John Smith",
-//                   prefixIcon: Icon(
-//                     CupertinoIcons.info,
-//                     color: nameFoucsNode.hasFocus
-//                         ? Theme.of(context).colorScheme.primary
-//                         : Colors.black54,
-//                   ),
-//                   label: const Text(
-//                     "About",
-//                     style: TextStyle(
-//                       fontSize: 25,
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
 class _ProfileScreenState extends State<ProfileScreen> {
   final FocusNode nameFocusNode = FocusNode();
   final FocusNode aboutFocusNode = FocusNode();
-
   @override
   void dispose() {
     nameFocusNode.dispose();
@@ -137,13 +27,14 @@ class _ProfileScreenState extends State<ProfileScreen> {
     //
     final mediaQ = MediaQuery.of(context).size;
     final heightForCircularImage = mediaQ.height * 0.20;
+    final primaryThemeColor = Theme.of(context).colorScheme.primary;
     //
     return Scaffold(
       appBar: AppBar(
         leading: Icon(Icons.home_outlined),
         title: Text("User Profile"),
       ),
-      body: Padding(
+      body: SingleChildScrollView(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
@@ -169,10 +60,11 @@ class _ProfileScreenState extends State<ProfileScreen> {
             SizedBox(
               height: mediaQ.height * 0.020,
             ),
-            Text(widget.user_Info.email, style: TextStyle(color: Colors.black54)),
+            Text(widget.user_Info.email,
+                style: TextStyle(color: Colors.black54)),
             //
             SizedBox(
-              height: mediaQ.height * 0.020,
+              height: mediaQ.height * 0.040,
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
@@ -180,29 +72,36 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 focusNode: nameFocusNode,
                 initialValue: widget.user_Info.name,
                 // style: TextStyle(
-                //   color: nameFocusNode.hasFocus ? Colors.amber : Colors.black,
+                //   color: nameFocusNode.hasFocus ? primaryThemeColor : Colors.black,
                 // ),
                 decoration: InputDecoration(
                   enabledBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(15),
                     borderSide: BorderSide(
-                      color: nameFocusNode.hasFocus ? Colors.amber : Colors.black54,
+                      color: nameFocusNode.hasFocus
+                          ? primaryThemeColor
+                          : Colors.black54,
                     ),
                   ),
                   focusedBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(15),
                     borderSide: BorderSide(
-                      color: Colors.amber,
+                      color: primaryThemeColor,
                     ),
                   ),
                   hintText: "i.e : John Smith",
                   prefixIcon: Icon(
                     CupertinoIcons.person,
-                    color: nameFocusNode.hasFocus ? Colors.amber : Colors.black54,
+                    color: nameFocusNode.hasFocus
+                        ? primaryThemeColor
+                        : Colors.black54,
+                    // color: primaryThemeColor,
                   ),
                   labelText: "Name",
                   labelStyle: TextStyle(
-                    color: nameFocusNode.hasFocus ? Colors.amber : Colors.black54,
+                    color: nameFocusNode.hasFocus
+                        ? primaryThemeColor
+                        : Colors.black54,
                     fontSize: 25,
                   ),
                 ),
@@ -212,6 +111,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 },
               ),
             ),
+            //
             SizedBox(
               height: mediaQ.height * 0.020,
             ),
@@ -220,30 +120,36 @@ class _ProfileScreenState extends State<ProfileScreen> {
               child: TextFormField(
                 focusNode: aboutFocusNode,
                 initialValue: widget.user_Info.about,
-                style: TextStyle(
-                  color: aboutFocusNode.hasFocus ? Colors.amber : Colors.black,
-                ),
+                // style: TextStyle(
+                //   color: aboutFocusNode.hasFocus ? primaryThemeColor : Colors.black,
+                // ),
                 decoration: InputDecoration(
                   enabledBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(15),
                     borderSide: BorderSide(
-                      color: aboutFocusNode.hasFocus ? Colors.amber : Colors.black54,
+                      color: aboutFocusNode.hasFocus
+                          ? primaryThemeColor
+                          : Colors.black54,
                     ),
                   ),
                   focusedBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(15),
                     borderSide: BorderSide(
-                      color: Colors.amber,
+                      color: primaryThemeColor,
                     ),
                   ),
                   hintText: "i.e : John Smith",
                   prefixIcon: Icon(
                     CupertinoIcons.info,
-                    color: aboutFocusNode.hasFocus ? Colors.amber : Colors.black54,
+                    color: aboutFocusNode.hasFocus
+                        ? primaryThemeColor
+                        : Colors.black54,
                   ),
                   labelText: "About",
                   labelStyle: TextStyle(
-                    color: aboutFocusNode.hasFocus ? Colors.amber : Colors.black54,
+                    color: aboutFocusNode.hasFocus
+                        ? primaryThemeColor
+                        : Colors.black54,
                     fontSize: 25,
                   ),
                 ),
@@ -253,8 +159,28 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 },
               ),
             ),
+            //
+            SizedBox(
+              height: mediaQ.height * 0.050,
+            ),
+            ElevatedButton.icon(
+                onPressed: () {},
+                icon: Icon(Icons.edit),
+                label: Text("Update Profile"),
+                style: ElevatedButton.styleFrom(
+                    minimumSize: Size(mediaQ.width * 0.7, mediaQ.height * .06),
+                    // shape: RoundedRectangleBorder(
+                    //   borderRadius: BorderRadius.circular(15),
+                    // ),
+                    shape: StadiumBorder()))
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.logout),
+        backgroundColor: Colors.white,
+        foregroundColor: primaryThemeColor,
       ),
     );
   }
