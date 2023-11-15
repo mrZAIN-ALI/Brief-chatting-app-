ee9d210f85ac685ad2cdc35e134ad47476957faa
ZAIN ALI <97796695+mrZAIN-ALI@users.noreply.github.com>
2023-11-15 05:31:59 +0500

    finding error which is createing two users when signing up

--- lib/screens/splash_Screen.dart
+++ lib/screens/splash_Screen.dart
@@ -4,7 +4,6 @@ import 'package:chit_chat/screens/home_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_launcher_icons/xml_templates.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
