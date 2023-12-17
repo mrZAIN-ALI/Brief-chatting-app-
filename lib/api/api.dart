8053dd8c3d261642553f9ba94dcc80ce632f3064
ZAIN ALI <97796695+mrZAIN-ALI@users.noreply.github.com>
2023-12-17 20:36:06 +0500

    First Commit on GETX

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -34,14 +34,7 @@ class Apis {
         }
       },
     );
-    // FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-    //   print('Got a message whilst in the foreground!');
-    //   print('Message data: ${message.data}');
 
-    //   if (message.notification != null) {
-    //     print('Message also contained a notification: ${message.notification}');
-    //   }
-    // });
   }
 
   //
